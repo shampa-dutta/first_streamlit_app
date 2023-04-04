7c4839dbfb4fd53f6bfe767eeb52bf45e717f1ac
shampa-dutta <58931611+shampa-dutta@users.noreply.github.com>
2023-04-05 01:35:54 +0530

    Update streamlit_app.py
    made changes in text
    added new headers

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,10 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 and Oatmeal')
-streamlit.text('Abc')
-streamlit.text('xyz')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado toast')
+
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
